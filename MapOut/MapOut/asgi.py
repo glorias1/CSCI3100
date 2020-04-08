b0ad4b5349d0e37e9fe5d08f19c379f03ce5b5e9
winniechang0 <48830634+winniechang0@users.noreply.github.com>
2020-04-08 16:49:24 +0800

    .

--- MapOut/MapOut/asgi.py
+++ MapOut/MapOut/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MapOut_site project.
+ASGI config for MapOut project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MapOut_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MapOut.settings')
 
 application = get_asgi_application()
--- MapOut/MapOut/wsgi.py
+++ MapOut/MapOut/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MapOut_site project.
+WSGI config for MapOut project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MapOut_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MapOut.settings')
 
 application = get_wsgi_application()
