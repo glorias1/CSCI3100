198b454268021660dd3b617d52880d8ddf875c2d
winniechang0 <48830634+winniechang0@users.noreply.github.com>
2020-04-09 20:05:47 +0800

    trying to make login views and models (edit laterrr))

--- MapOut/mapout_app/models.py
+++ MapOut/mapout_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class usertable(models.Model):
+    username = models.CharField(max_length=30,default='')
+    email = models.CharField(max_length=255,default='')
+    password = models.CharField(max_length=16, default='')
+    def __str__(self):
+        return self.username + self.email
