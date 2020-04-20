7697c655d4033bef9b8ba15b052402468c081649
MangChun07 <49334289+MangChun07@users.noreply.github.com>
2020-04-20 20:54:25 +0800

    終於add到
    另外開左個profile model, 暫時得privacy, 睇下之後加唔加user profile哩樣野, 可以用到

--- MapOut/mapout_app/admin.py
+++ MapOut/mapout_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(User_Privacy)
+admin.site.register(Profile)
 admin.site.register(Project)
 admin.site.register(Tasks)
 admin.site.register(Chat)
--- MapOut/mapout_app/models.py
+++ MapOut/mapout_app/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 # Create your models here.
-class User_Privacy(models.Model):
-    #user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.ForeignKey(User, on_delete=models.CASCADE)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #username = models.ForeignKey(User, on_delete=models.CASCADE)
     private = models.BooleanField(default=False)
 
-    def __unicode__(self):
-        return {self.username, self.private}
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
 
 class Project(models.Model):
     project_name = models.CharField(max_length=100)
