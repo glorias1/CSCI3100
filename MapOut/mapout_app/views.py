e671e7069269fa363168619c92eab97ee802a742
MangChun07 <49334289+MangChun07@users.noreply.github.com>
2020-04-21 00:25:47 +0800

    doing public area
    doing public area

--- MapOut/mapout_app/views.py
+++ MapOut/mapout_app/views.py
@@ -29,8 +29,10 @@ def index(request):
 def index_projects(request):
     #get the projects involed by the current user
     projects = Project.objects.filter(members = request.user).order_by('closed')
+    all_public_project = Project.objects.filter(private = False).order_by('project_name')
+    print(all_public_project)
     tasks = Tasks.objects.filter(belong_project__in = projects)  ##filter all the task in the list of project objects
-    context = {'projects':projects, 'tasks':tasks}
+    context = {'projects':projects, 'tasks':tasks, 'all_public_project': all_public_project}
     return render(request, 'main_projects.html', context)
 
 def index_tasks(request):
