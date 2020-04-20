6d087d1e4acabb84e37387a36aeb1a93358ff459
MangChun07 <49334289+MangChun07@users.noreply.github.com>
2020-04-20 16:25:36 +0800

    搞緊privacy setting
    搞緊privacy setting

--- MapOut/mapout_app/admin.py
+++ MapOut/mapout_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+admin.site.register(User_Privacy)
 admin.site.register(Project)
 admin.site.register(Tasks)
 admin.site.register(Chat)
--- MapOut/mapout_app/views.py
+++ MapOut/mapout_app/views.py
@@ -62,14 +62,15 @@ def signup_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user.refresh_from_db()
-            user.profile.private = form.cleaned_data.get('privacy')
-            user.save()
             user = authenticate(username=username, password=raw_password)
             login(request, user)
+            new_registration = User_Privacy(
+                username = user,
+                private = form.cleaned_data.get('privacy')
+            )
             return redirect('home')
     else:
         form = SignUpForm()
