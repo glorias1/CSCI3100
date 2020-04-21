3883f7e46b1186b482326206bca76034a08ed77f
glorias1 <49317051+glorias1@users.noreply.github.com>
2020-04-21 15:02:45 +0800

    try haha

--- MapOut/MapOut/settings.py
+++ MapOut/MapOut/settings.py
@@ -120,6 +120,15 @@ USE_L10N = True
 USE_TZ = True
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 0
+EMAIL_HOST_USER = 'wendywong.cuhk@gmail.com'
+EMAIL_HOST_PASSWORD = 'Viqko8-dichab-cyfsod'
+EMAIL_USE_TLS = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+EMAIL_USE_SSL = False
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
