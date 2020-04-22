5e6068acbe5e06606a734cf826b976b7bd697607
winniechang0 <48830634+winniechang0@users.noreply.github.com>
2020-04-22 20:20:13 +0800

    show total capital and expense

--- MapOut/mapout_app/views.py
+++ MapOut/mapout_app/views.py
@@ -22,48 +22,22 @@ def index_budgets(request):
             return render(request, 'budget/create_plan.html')
     return render(request, 'budget/index.html')
 
+#this add capital
 def create_budget(request, id1):
     viewing_project = Project.objects.get(id = id1)
     viewing_plan = Budgetplan.objects.get(belong_project = viewing_project)
     context = {'viewing_project':viewing_project, 'viewing_plan':viewing_plan}
     if request.method == 'POST':
-        if request.POST.get('belong_project_select'):
-            selected_project_id = request.POST.get('belong_project_select')
-    
-            if (request.POST.get('capital_name') != '') and (request.POST.get('capital_amount') != ''):
-                transition_capital =            Budget()
-                transition_capital.transition_type = "capital"
-                transition_capital.belong_plan = viewing_plan
-                transition_capital.name =       request.POST.get('capital_name')
-                transition_capital.amount =     int(request.POST.get('capital_amount'))
-                transition_capital.save()
-    
-            if (request.POST.get('expense_1_name') != '') and (request.POST.get('expense_1_amount') != ''):
-                transition_expense_1 =          Budget()
-                transition_expense_1.transition_type = "expense"
-                transition_expense_1.belong_plan = viewing_plan
-                transition_expense_1.name =     request.POST.get('expense_1_name')
-                transition_expense_1.amount =   int(request.POST.get('expense_1_amount'))
-                transition_expense_1.save()
- 
-            if (request.POST.get('expense_2_name') != '') and (request.POST.get('expense_2_amount') != ''):       
-                transition_expense_2 =          Budget()
-                transition_expense_2.transition_type = "expense"
-                transition_expense_2.belong_plan = viewing_plan
-                transition_expense_2.name =     request.POST.get('expense_2_name')
-                transition_expense_2.amount =   int(request.POST.get('expense_2_amount'))
-                transition_expense_2.save()
-    
-            if (request.POST.get('expense_3_name') != '') and (request.POST.get('expense_3_amount') != ''):     
-                transition_expense_3 =          Budget()
-                transition_expense_3.transition_type = "expense"
-                transition_expense_3.belong_plan = viewing_plan
-                transition_expense_3.name =     request.POST.get('expense_3_name')
-                transition_expense_3.amount =   int(request.POST.get('expense_3_amount'))
-                transition_expense_3.save()
+        if request.POST.get('capital_name'):
+            transition_capital =          Budget()
+            transition_capital.transition_type = "capital"
+            transition_capital.belong_plan = viewing_plan # selected_project_id)
+            transition_capital.name =     request.POST.get('capital_name')
+            transition_capital.amount =   int(request.POST.get('capital_amount'))
+            transition_capital.save()
+    return render(request, 'budget/create_plan.html', context)
 
-    return render(request, 'budget/create_plan.html',context)
-
+##this is add expense
 def create_budget_2(request, id1):
     viewing_project = Project.objects.get(id = id1)
     viewing_plan = Budgetplan.objects.get(belong_project = viewing_project)
@@ -79,9 +53,26 @@ def create_budget_2(request, id1):
     return render(request, 'budget/create_plan_2.html', context)
 
 def view_budget(request, id3): # id3 is project id \
+    total_capital=0
+    total_expense=0
     viewing_project = Project.objects.get(id = id3)
     viewing_plan = Budgetplan.objects.get(belong_project = viewing_project)
     all_budget_records = Budget.objects.filter(belong_plan = viewing_plan)
+    try:
+        if viewing_project.members.get(id = request.user.id):
+            is_member = True
+    except:
+        is_member = False
+    ##filter out all capital/expense
+    all_budget = Budget.objects.filter(belong_plan = viewing_plan)
+    all_capital = all_budget.filter(transition_type = 'capital')
+    all_expense = all_budget.filter(transition_type = 'expense')
+
+    for one_capital in all_capital:
+        total_capital += one_capital.amount
+    for one_expense in all_expense:
+        total_expense += one_expense.amount
+    
     #if request.method == 'GET':
     #    total_expense = 0
     #    capital = Budget.objects.raw('SELECT * FROM mapout_app_budget WHERE belong_project_id = %s AND transition_type = "capital"', [id3])
@@ -93,7 +84,12 @@ def view_budget(request, id3): # id3 is project id \
         #"expense": total_expense,
         "viewing_project": viewing_project,
         "all_budget_records": all_budget_records,
-        "viewing_plan":viewing_plan
+        "viewing_plan":viewing_plan,
+        "is_member":is_member,
+        "all_capital":all_capital,
+        "all_expense":all_expense,
+        "total_capital":total_capital,
+        "total_expense":total_expense
     }
         #return render(request, 'budget/view_plan.html', context)
     return render(request, 'budget/view_plan.html',context)
